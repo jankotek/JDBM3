59f76fff09d9c966f7a10160169b3ac5d4e3409f
Jan Kotek <jan@kotek>
2010-07-14 23:57:47 +0000

    failing test for defrag

--- src/tests/jdbm/recman/TestDefrag.java
+++ src/tests/jdbm/recman/TestDefrag.java
@@ -1,6 +1,9 @@
 package jdbm.recman;
 
 import java.io.IOException;
+import java.util.TreeMap;
+
+import jdbm.PrimaryTreeMap;
 
 public class TestDefrag extends TestCaseWithTestFile{
 
@@ -13,5 +16,24 @@ public class TestDefrag extends TestCaseWithTestFile{
 		m.close();
 		m = new BaseRecordManager(file);
 		assertEquals(m.fetch(loc),"123");
+	}
+	
+
+	
+	public void testDefragBtree() throws IOException{
+		String file = newTestFile();
+		BaseRecordManager m = new BaseRecordManager(file);
+		PrimaryTreeMap t = m.treeMap("aa");
+		TreeMap t2 = new TreeMap();
+		for(int i =0;i<10000;i ++ ){
+			t.put(i, ""+i);
+			t2.put(i, ""+i);
+		}
+					
+		m.defrag();		
+		m.close();
+		m = new BaseRecordManager(file);
+		t = m.treeMap("aa");
+		assertEquals(t,t2);
 	}
 }
