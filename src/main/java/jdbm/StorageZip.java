14847db6d663c937822bdcceaa4f5687e94d59be
Jan Kotek <jan@artemis.(none)>
2011-12-08 19:39:37 +0000

    First shot on implementing readonly ZIP storage

---
+++ src/main/java/jdbm/StorageZip.java
@@ -0,0 +1,11 @@
+package jdbm;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jan
+ * Date: 08/12/11
+ * Time: 08:12
+ * To change this template use File | Settings | File Templates.
+ */
+public class StorageZip {
+}
---
+++ src/test/java/jdbm/StorageZipTest.java
@@ -0,0 +1,11 @@
+package jdbm;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jan
+ * Date: 08/12/11
+ * Time: 07:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class StorageZipTest {
+}
