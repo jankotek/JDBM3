bd674fcb57d3cfd8d40e29471077f75bf901b8e6
Jan Kotek <jan@artemis.(none)>
2011-12-08 20:27:41 +0000

    just another meaning less commit

---
+++ src/test/java/jdbm/HugeData.java
@@ -0,0 +1,11 @@
+package jdbm;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jan
+ * Date: 08/12/11
+ * Time: 10:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class HugeData {
+}
