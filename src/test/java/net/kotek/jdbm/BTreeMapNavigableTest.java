5914dc012b63dbb905c1d6ff70005cec93efcfc0
Jan Kotek <jan@artemis.(none)>
2012-03-03 20:42:07 +0000

    Got permission from Luc Peuvrier for Apache 2 license

--- src/test/java/net/kotek/jdbm/BTreeMapNavigableTest.java
+++ src/test/java/net/kotek/jdbm/BTreeMapNavigableTest.java
@@ -1,19 +1,47 @@
 /*
- * Copyright 2007,2008 Luc Peuvrier
- * 
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE
- * Licensed under the LGPL License, Version 3, 29 June 2007 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.gnu.org/licenses/lgpl.html
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright 2012 Luc Peuvrier
+* All rights reserved.
+*
+* This file is a part of JOAFIP.
+*
+* JOAFIP is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License.
+*
+* Licensed under the GNU LESSER GENERAL PUBLIC LICENSE
+* Licensed under the LGPL License, Version 3, 29 June 2007 (the "LGPL License");
+* you may not use this file except in compliance with the "LGPL License" extended with here below additional permissions.
+* You may obtain a copy of the "LGPL License" at
+*
+*    http://www.gnu.org/licenses/lgpl.html
+*
+* Additional permissions extensions for this file:
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted under the the Apache License, Version 2.0 (the "Apache License") instead of the "LGPL License"
+* and if following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in the
+* documentation and/or other materials provided with the distribution.
+*
+* 3. Neither the name of the copyright holders nor the names of its
+* contributors may be used to endorse or promote products derived from
+* this software without specific prior written permission.
+*
+* You may obtain a copy of the "Apache License" at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* JOAFIP is distributed in the hope that it will be useful, but
+* unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package net.kotek.jdbm;
 
 import java.io.IOException;
