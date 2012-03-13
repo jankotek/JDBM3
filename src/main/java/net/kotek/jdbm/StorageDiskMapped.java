cb04445a2506a3fd749a288374330f67f29d4578
Jan Kotek <jan@artemis.(none)>
2012-03-13 00:42:09 +0000

    Revert: Small optimalization for syncing only modified files (a78df4799ebc30534276291b4e7a4e9f72d6c76a)
    Also fixes Issue #22

--- src/main/java/net/kotek/jdbm/StorageDiskMapped.java
+++ src/main/java/net/kotek/jdbm/StorageDiskMapped.java
@@ -33,8 +33,6 @@ class StorageDiskMapped implements Storage {
     private ArrayList<FileChannel> channels = new ArrayList<FileChannel>();
     private ArrayList<FileChannel> channelsTranslation = new ArrayList<FileChannel>();
     private IdentityHashMap<FileChannel, MappedByteBuffer> buffers = new IdentityHashMap<FileChannel, MappedByteBuffer>();
-    
-    private IdentityHashMap<MappedByteBuffer,Void> buffersToSync = new IdentityHashMap<MappedByteBuffer,Void>();
 
     private String fileName;
     private boolean transactionsDisabled;
@@ -117,7 +115,6 @@ class StorageDiskMapped implements Storage {
         b.position(offsetInFile);
         data.rewind();
         b.put(data);
-        buffersToSync.put(b,null);
     }
 
     private void unmapBuffer(MappedByteBuffer b) {
@@ -172,10 +169,9 @@ class StorageDiskMapped implements Storage {
     }
 
     public void sync() throws IOException {
-        for(MappedByteBuffer b: buffersToSync.keySet()){
+        for(MappedByteBuffer b: buffers.values()){
             b.force();
         }
-        buffersToSync.clear();
     }
 
 
